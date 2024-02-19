96e7bdd31271d0a58e4ffb12316c45e0c75caeb1
KaineBinch <kainebinch@live.com>
2024-02-19 01:38:56 +0000

    feat: added fontawesome icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { HashRouter } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faPhone);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
