e71603b775d918d8b0acb4b38067e5c0677ed463
KaineBinch <kainebinch@live.com>
2024-02-21 01:00:29 +0000

    chore: removed blur

--- src/components/wip.jsx
+++ src/components/wip.jsx
@@ -3,13 +3,13 @@ import { wipPercentage } from "../constants/constants";
 const Wip = ({ showButton = true }) => {
   return (
     <div
-      className="hero min-h-screen"
+      className="hero min-h-svh"
       style={{
         backgroundImage:
           "url(https://images.unsplash.com/photo-1517089596392-fb9a9033e05b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
       }}
     >
-      <div className="hero-overlay bg-opacity-60 backdrop-blur-[2px]"></div>
+      <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <div
