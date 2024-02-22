b0f2044426fdfa60a66be90cfdca4e499c9e781f
KaineBinch <kainebinch@live.com>
2024-02-22 01:30:32 +0000

    feat: created logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+const Logo = () => {
+  return (
+    <div className="text-white text-lg font-[arialBlack] lg:text-2xl md:text-xl">
+      <h1>Express</h1>
+      <h1 className="bg-secondary text-sm px-2 py-1 -mb-3 lg:-mb-6 lg:text-lg md:-mb-3 md:text-md">
+        Tool Hire Ltd
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
