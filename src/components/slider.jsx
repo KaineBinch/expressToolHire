e3530c76d9d47b4084cb33fce0b70a0ca468ca68
KaineBinch <kainebinch@live.com>
2024-02-23 05:05:00 +0000

    feat: edited slider

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState, useRef } from "react";
-
 const featuredImages = [
   "./src/assets/sliderImages/Digger.png",
   "./src/assets/sliderImages/Diggers.png",
@@ -9,54 +6,17 @@ const featuredImages = [
   "./src/assets/sliderImages/Scaffold-hoist.png",
 ];
 
-let count = 0;
-let slideInterval;
-
-export default function Slider() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const slideRef = useRef();
-
-  const removeAnimation = () => {
-    slideRef.current.classList.remove("fade-anim");
-  };
-
-  useEffect(() => {
-    slideRef.current.addEventListener("animationend", removeAnimation);
-
-    startSlider();
-    return () => {
-      pauseSlider();
-    };
-  }, []);
-
-  const startSlider = () => {
-    slideInterval = setInterval(() => {
-      handleOnNextClick();
-    }, 5000);
-  };
-
-  const pauseSlider = () => {
-    clearInterval(slideInterval);
-  };
-
-  const handleOnNextClick = () => {
-    count = (count + 1) % featuredImages.length;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
-  };
-
+const Slider = () => {
   return (
-    <div
-      ref={slideRef}
-      className="flex justify-center items-center align-center w-full mt-[25px] mb-[25px] md:mt-[100px] md:mb-[100px] px-[10px] select-none relative"
-    >
-      <div className="aspect-w-16 aspect-h-9">
-        <img
-          src={featuredImages[currentIndex]}
-          alt=""
-          className="md:max-h-[800px] md:max-w-[800px]"
-        />
+    <div className="carousel w-full">
+      <div className="carousel-item w-full">
+        {featuredImages.map((src, i) => (
+          <div key={i} className="carousel-item w-full">
+            <img src={src} className="w-full" />
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
+export default Slider;
